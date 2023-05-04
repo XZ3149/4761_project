cde70495e7a83afe3973bc315ab2fdc1b018cac1
XZ3149 <114506836+XZ3149@users.noreply.github.com>
2023-05-03 23:31:40 -0400

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget -b https://www.encodeproject.org/files/ENCFF954ZUO/@@download/ENCFF954ZUO.bam -O ctcf.bam
+wget -b https://www.encodeproject.org/files/ENCFF222DUW/@@download/ENCFF222DUW.bam -O h3k9me3.bam
+wget -b https://www.encodeproject.org/files/ENCFF197VOO/@@download/ENCFF197VOO.bam -O dnase.bam
+wget -b https://www.encodeproject.org/files/ENCFF874CFD/@@download/ENCFF874CFD.bam -O rna.bam
+
